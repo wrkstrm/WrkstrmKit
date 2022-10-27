00b27286385635e02b23b6e3b3fb64a44466596b
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2022-10-26 19:56:20 -0700

    Fix CodeSwiftly output.

--- Package.swift
+++ Package.swift
@@ -1,12 +1,8 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
   name: "WrkstrmKit",
-  // Products define the executables and libraries produced by a package, and make them visible to
-  // other packages.
   platforms: [
     .iOS(.v13),
     .macOS(.v12),
