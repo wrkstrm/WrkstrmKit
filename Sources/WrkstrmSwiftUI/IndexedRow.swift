33ccaf336d72ad2823ec27b2a1b4de80a9e63cc3
Cristian A Monterroza <1240948+rismay@users.noreply.github.com>
2024-06-28 00:08:44 -0700

    Update IndexedRow.swift

--- Sources/WrkstrmSwiftUI/IndexedRow.swift
+++ Sources/WrkstrmSwiftUI/IndexedRow.swift
@@ -7,7 +7,7 @@ public final class IndexedRow<M: Identifiable>: Identifiable, ObservableObject {
 
   @Published public var element: M
 
-  @_specialize(where M:_Trivial)
+  @_specialize(where M: _Trivial)
   public init(id: Int, index: Int, element: M) {
     self.id = id
     self.index = index
