7073fdb542f075f6fe5ec990438aa522bab94f25
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-11-14 00:56:32 -0800

    Update IndexedRow.swift
    Remove Identifiable requirement from generic argument.

--- Sources/WrkstrmSwiftUI/IndexedRow.swift
+++ Sources/WrkstrmSwiftUI/IndexedRow.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public final class IndexedRow<M: Identifiable>: Identifiable, ObservableObject {
+public final class IndexedRow<M>: Identifiable, ObservableObject {
   public let id: Int
 
   public let index: Int
