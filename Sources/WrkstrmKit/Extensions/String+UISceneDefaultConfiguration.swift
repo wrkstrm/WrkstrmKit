94ae528d53a7e7b92b149a5735195793774c3eaf
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-11-21 14:02:29 -0800

    Create String+UISceneDefaultConfiguration.swift

---
+++ Sources/WrkstrmKit/Extensions/String+UISceneDefaultConfiguration.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension String {
+  /// The default configuration name for a UIScene. Related to `UISceneConfigurationName`.
+  /// Expected to match the Info.plist file for default configurations.
+  public static let defaultConfiguration: Self = "Default Configuration"
+}
