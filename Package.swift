083bf073352483fe9dfc872465dfdde1c8fd9e95
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-12 20:35:21 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ extension PackageDescription.Package.Dependency {
   static var remote: [PackageDescription.Package.Dependency] {
     [
       .package(url: "https://github.com/wrkstrm/WrkstrmFoundation.git", branch: "main"),
-      .package(url: "https://github.com/wrkstrm/WrkstrmLog.git", branch: "main"),
+      .package(url: "https://github.com/wrkstrm/WrkstrmLog.git", from: "2.0.0"),
       .package(url: "https://github.com/wrkstrm/WrkstrmMain.git", from: "2.0.0"),
     ]
   }
