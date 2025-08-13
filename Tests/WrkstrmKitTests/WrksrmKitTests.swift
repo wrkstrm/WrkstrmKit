253803be1ffff57679e9231fe57151ab8c13a151
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-13 13:18:20 -0700

    refactor: migrate tests to Swift Testing

--- Tests/WrkstrmKitTests/WrksrmKitTests.swift
+++ Tests/WrkstrmKitTests/WrksrmKitTests.swift
@@ -1,14 +1,11 @@
-import XCTest
+import Testing
 
 @testable import WrkstrmKit
 
-final class WrkstrmKitTests: XCTestCase {
-  func testExample() {
-    // This is an example of a functional test case. Use XCTAssert and related functions to verify
-    // your tests produce the correct results.
-    let text = "Hello, World!"
-    XCTAssertEqual(text, "Hello, World!")
-  }
-
-  static var allTests = [("testExample", testExample)]
+@Test
+func testExample() {
+  // This is an example of a functional test case. Use #expect and related functions to verify
+  // your tests produce the correct results.
+  let text = "Hello, World!"
+  #expect(text == "Hello, World!")
 }
--- Tests/WrkstrmKitTests/XCTestManifests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-  public func allTests() -> [XCTestCaseEntry] {
-    [testCase(WrkstrmKitTests.allTests)]
-  }
-#endif
