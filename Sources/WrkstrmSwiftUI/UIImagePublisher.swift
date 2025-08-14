32e898d3e43ee44c945916653cb093844ebbde45
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-13 17:40:48 -0700

    Add documentation for SwiftUI utilities

--- Sources/WrkstrmSwiftUI/UIImagePublisher.swift
+++ Sources/WrkstrmSwiftUI/UIImagePublisher.swift
@@ -3,11 +3,25 @@
   import SwiftUI
   import UIKit
 
+  /// An observable object that publishes updates to a `UIImage` for use in SwiftUI views.
+  ///
+  /// ```swift
+  /// let publisher = UIImagePublisher(UIImage(named: "Logo")!)
+  /// Image(uiImage: publisher.uiImage)
+  /// ```
   open class UIImagePublisher: ObservableObject {
+    /// A cancellable reference to an image loading task.
     public var imageCancellable: AnyCancellable?
 
+    /// The currently published image.
     @Published public var uiImage: UIImage
 
+    /// Creates a publisher with an initial image.
+    /// - Parameter uiImage: The starting image to publish.
+    /// - Example:
+    ///   ```swift
+    ///   let publisher = UIImagePublisher(UIImage(named: "Avatar")!)
+    ///   ```
     public init(_ uiImage: UIImage) {
       self.uiImage = uiImage
     }
