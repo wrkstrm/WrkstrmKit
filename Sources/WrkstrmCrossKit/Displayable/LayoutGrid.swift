b29bf8a0ddfa602ff12f6b1a68624f60610d4588
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-20 17:11:53 -0700

    Rename Grid to LayoutGrid (#20)

--- Sources/WrkstrmCrossKit/Displayable/LayoutGrid.swift
+++ Sources/WrkstrmCrossKit/Displayable/LayoutGrid.swift
@@ -1,7 +1,11 @@
 #if canImport(CoreGraphics)
 import CoreGraphics
 
-public struct Grid {
+/// A lightweight model describing a two-dimensional layout used to calculate
+/// item sizes for grid-based interfaces. This type is unrelated to SwiftUI's
+/// `Grid` view; it simply specifies the number of rows and columns available
+/// for placing content within a given frame.
+public struct LayoutGrid {
   public let rows: Int
 
   public let columns: Int
